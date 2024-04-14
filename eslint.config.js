faf8070e6fb5ffd66e9c3a01110be0635e2e40b6
Dmitry Maslennikov <maslennikovdm@gmail.com>
2024-04-14 23:38:08 +0300

    MIgrate to new eslint and stylistic

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import stylistic from '@stylistic/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tsEslint from 'typescript-eslint'
+
+export default [
+    {
+        files: [
+            'src/**/*.ts?(x)',
+            'utils/**/*.ts?(x)',
+            '*.js'
+        ],
+        languageOptions: {
+            parser,
+            parserOptions: { project: './tsconfig.json' }
+        }
+    },
+    ...tsEslint.configs.recommended,
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: 'single',
+        semi: false,
+        jsx: true,
+        commaDangle: 'never'
+    }),
+    {
+        rules: {
+            '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+            '@stylistic/jsx-curly-brace-presence': ['error', 'always'],
+            '@stylistic/jsx-wrap-multilines': 'off',
+            '@stylistic/jsx-closing-tag-location': 'off'
+        }
+    },
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort
+        },
+        rules: {
+            'simple-import-sort/imports': 'error'
+        }
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-floating-promises': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true
+                }
+            ]
+        }
+    }
+]
