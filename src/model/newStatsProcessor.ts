1d2a3af70f6bd9c6bc081fa11221d8748fa8d4c8
Dmitry Maslennikov <maslennikovdm@gmail.com>
2024-04-20 17:00:02 +0300

    Simplyfy new stats code

--- src/model/newStatsProcessor.ts
+++ src/model/newStatsProcessor.ts
@@ -10,19 +10,19 @@ const appState = AppState.instance()
 const operationsModel = OperationsModel.instance()
 
 interface Interval {
-    start: DateTime
-    isFirstDay: boolean
-    isPast: boolean
-    isNow: boolean
-    isFuture: boolean
+    readonly start: DateTime
+    readonly isFirstDay: boolean
+    readonly isPast: boolean
+    readonly isNow: boolean
+    readonly isFuture: boolean
 }
 
 export interface Intervals {
-    day: Interval
-    sWeek: Interval
-    mWeek: Interval
-    month: Interval
-    year: Interval
+    readonly day: Interval
+    readonly sWeek: Interval
+    readonly mWeek: Interval
+    readonly month: Interval
+    readonly year: Interval
 }
 
 export class StatsReducer {
@@ -49,85 +49,9 @@ export async function calcStats2(
         return [operationsModel.firstOp?.date ?? appState.today, operationsModel.lastOp?.date ?? appState.today]
     })()
 
-    const intervals: Intervals = {
-        day: {
-            start: startDate,
-            isFirstDay: true,
-            isPast: true,
-            isNow: true,
-            isFuture: true
-        },
-        sWeek: {
-            start: startDate.weekday === 7 ? startDate : startDate.minus({ days: startDate.weekday }),
-            isFirstDay: true,
-            isPast: true,
-            isNow: true,
-            isFuture: true
-        },
-        mWeek: {
-            start: startDate.startOf('week'),
-            isFirstDay: true,
-            isPast: true,
-            isNow: true,
-            isFuture: true
-        },
-        month: {
-            start: startDate.startOf('month'),
-            isFirstDay: true,
-            isPast: true,
-            isNow: true,
-            isFuture: true
-        },
-        year: {
-            start: startDate.startOf('year'),
-            isFirstDay: true,
-            isPast: true,
-            isNow: true,
-            isFuture: true
-        }
-    }
-
     let index = 0
     for (let date = startDate; date <= endDate; date = date.plus({ day: 1 })) {
-        // intervals.day.isFirstDay = true
-        intervals.day.start = date
-        intervals.sWeek.isFirstDay = date.weekday === 7
-        if (intervals.sWeek.isFirstDay) {
-            intervals.sWeek.start = date
-        }
-        intervals.mWeek.isFirstDay = date.weekday === 1
-        if (intervals.mWeek.isFirstDay) {
-            intervals.mWeek.start = date
-        }
-        intervals.month.isFirstDay = date.day === 1
-        if (intervals.month.isFirstDay) {
-            intervals.month.start = date
-        }
-        intervals.year.isFirstDay = date.month === 1 && date.day === 1
-        if (intervals.year.isFirstDay) {
-            intervals.year.start = date
-        }
-
-        intervals.day.isFuture = today < intervals.day.start
-        intervals.day.isPast = today > intervals.day.start
-        intervals.day.isNow = !intervals.day.isFuture && !intervals.day.isPast
-
-        intervals.sWeek.isFuture = today < intervals.sWeek.start
-        intervals.sWeek.isPast = intervals.sWeek.start.plus({ days: 6 }) < today
-        intervals.sWeek.isNow = !intervals.sWeek.isFuture && !intervals.sWeek.isPast
-
-        intervals.mWeek.isFuture = today < intervals.mWeek.start
-        intervals.mWeek.isPast = intervals.mWeek.start.plus({ days: 6 }) < today
-        intervals.mWeek.isNow = !intervals.mWeek.isFuture && !intervals.mWeek.isPast
-
-        intervals.month.isFuture = today < intervals.month.start
-        intervals.month.isPast = intervals.month.start.plus({ month: 1 }).minus({ day: 1 }) < today
-        intervals.month.isNow = !intervals.month.isFuture && !intervals.month.isPast
-
-        intervals.year.isFuture = today.year < intervals.year.start.year
-        intervals.year.isPast = intervals.year.start.year < today.year
-        intervals.year.isNow = !intervals.year.isFuture && !intervals.year.isPast
-
+        const intervals = calcIntervals(date, today)
         reducers.forEach(reducer => reducer.newDay(intervals))
 
         while (index < operationsModel.operations.length) {
@@ -158,4 +82,74 @@ export async function calcStats2(
     }
 
     reducers.forEach(r => r.done())
+}
+
+function calcIntervals(date: DateTime, today: DateTime): Intervals {
+    const sWeekFirstDay = date.weekday === 7
+    const mWeekFirstDay = date.weekday === 1
+    const monthFirstDay = date.day === 1
+    const yearFirstDay = date.month === 1 && date.day === 1
+
+    const sWeekStart = date.weekday === 7 ? date : date.minus({ days: date.weekday })
+    const mWeekStart = date.startOf('week')
+    const monthStart = date.startOf('month')
+    const yearStart = date.startOf('year')
+
+    const dayFuture = today < date
+    const dayPast = today > date
+    const dayNow = !dayFuture && !dayPast
+
+    const sWeekFuture = today < sWeekStart
+    const sWeekPast = sWeekStart.plus({ days: 6 }) < today
+    const sWeekNow = !sWeekFuture && !sWeekPast
+
+    const mWeekFuture = today < mWeekStart
+    const mWeekPast = mWeekStart.plus({ days: 6 }) < today
+    const mWeekNow = !mWeekFuture && !mWeekPast
+
+    const monthFuture = today < monthStart
+    const monthPast = monthStart.plus({ month: 1 }).minus({ day: 1 }) < today
+    const monthNow = !monthFuture && !monthPast
+
+    const yearFuture = today.year < yearStart.year
+    const yearPast = yearStart.year < today.year
+    const yearNow = !yearFuture && !yearPast
+
+    return {
+        day: {
+            start: date,
+            isFirstDay: true,
+            isPast: dayPast,
+            isNow: dayNow,
+            isFuture: dayFuture
+        },
+        sWeek: {
+            start: sWeekStart,
+            isFirstDay: sWeekFirstDay,
+            isPast: sWeekPast,
+            isNow: sWeekNow,
+            isFuture: sWeekFuture
+        },
+        mWeek: {
+            start: mWeekStart,
+            isFirstDay: mWeekFirstDay,
+            isPast: mWeekPast,
+            isNow: mWeekNow,
+            isFuture: mWeekFuture
+        },
+        month: {
+            start: monthStart,
+            isFirstDay: monthFirstDay,
+            isPast: monthPast,
+            isNow: monthNow,
+            isFuture: monthFuture
+        },
+        year: {
+            start: yearStart,
+            isFirstDay: yearFirstDay,
+            isPast: yearPast,
+            isNow: yearNow,
+            isFuture: yearFuture
+        }
+    }
 }
