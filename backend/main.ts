e0836f4ffb58586bb9a8bce538e4aa6267cba73b
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-01-02 13:27:00 +0300

    Use @fastify/static and fastify-acme

--- backend/main.ts
+++ backend/main.ts
@@ -2,27 +2,27 @@ import { fastifyCompress } from '@fastify/compress'
 import { fastifyCors } from '@fastify/cors'
 import { fastifyResponseValidation } from '@fastify/response-validation'
 import { fastifySensible, httpErrors } from '@fastify/sensible'
+import staticPlugin from '@fastify/static'
 import formatsPlugin from 'ajv-formats'
 import { fastify, FastifyInstance, RawServerBase } from 'fastify'
+import { fastifyAcmeSecurePlugin, fastifyAcmeUnsecurePlugin, getCertAndKey, registerAcmeAccount } from 'fastify-acme'
 import { createReadStream } from 'fs'
-import { lstat } from 'fs/promises'
-import mime from 'mime-types'
 import path from 'path'
 
-import { AcmeClient } from './acme'
 import { registerAuthEndpoints } from './auth'
 import { registerManagementEndpoints } from './management'
 import { MongoStorage } from './storage/mongo'
 import { registerSyncEndpoints } from './sync'
 
 const settings = {
+    certDir: './auth',
     domain: 'app.cashmony.ru'
 }
 
 async function main() {
     if (process.argv[2] === 'acme-reg') {
-        const acme = await AcmeClient.create(undefined)
-        void acme.registerAccount('maslennikovdm@gmail.com')
+        await registerAcmeAccount(settings.certDir, 'maslennikovdm@gmail.com')
+        process.stdout.write('Done\n')
     } else {
         await initServer()
     }
@@ -30,49 +30,27 @@ async function main() {
 
 async function initServer() {
     const production = process.env.NODE_ENV === 'production'
-    const acmeTokens: Record<string, string> = {}
 
-    const unsecureServer = await initUnsecureServer(production, acmeTokens)
+    await initUnsecureServer(production)
 
     if (!production) {
         return
     }
 
-    const acme = await AcmeClient.create(unsecureServer.log.child({ module: 'Acme' }))
-
-    const getCert = () => {
-        return acme.cert(
-            settings.domain,
-            (token, content) => { acmeTokens[token] = content },
-            (token) => { delete acmeTokens[token] }
-        )
-    }
-    const cert = await getCert()
+    const certAndKey = await getCertAndKey(settings.certDir, settings.domain)
 
     const secureServer = fastify({
         http2: true,
         https: {
             allowHTTP1: true,
-            cert: cert.cert,
-            key: cert.key
+            cert: certAndKey.cert,
+            key: certAndKey.pkey
         },
         logger: true,
         ajv: { plugins: [formatsPlugin] }
     })
 
-    setInterval(
-        () => {
-            if (acme.shouldRenewCert()) {
-                (async () => {
-                    const cert = await getCert()
-                    secureServer.server.setSecureContext({ cert: cert.cert, key: cert.key })
-                })().catch((e): void => {
-                    secureServer.log.error(e, 'Error while update certificate')
-                })
-            }
-        },
-        1000 * 60 * 60 // 1 hour
-    )
+    secureServer.register(fastifyAcmeSecurePlugin, { domain: settings.domain, certDir: settings.certDir })
 
     await secureServer.register(fastifyCors, {
         methods: ['get', 'post']
@@ -83,10 +61,10 @@ async function initServer() {
 
     const mongo = await MongoStorage.create(secureServer.log.child({ module: 'MONGO' }))
 
-    registerStaticEndpoint(secureServer, 'web')
     registerAuthEndpoints(secureServer, mongo)
     registerSyncEndpoints(secureServer, mongo)
     registerManagementEndpoints(secureServer, mongo)
+    registerStaticEndpoint(secureServer, 'web')
 
     await secureServer.listen({
         host: production ? '::' : '::1',
@@ -94,45 +72,14 @@ async function initServer() {
     })
 }
 
-async function initUnsecureServer(production: boolean, acmeTokens: Record<string, string>) {
+async function initUnsecureServer(production: boolean) {
     const s = fastify({
         logger: true,
         ajv: { plugins: [formatsPlugin] }
     })
 
     if (production) {
-        s.get(
-            '/.well-known/acme-challenge/:token',
-            {},
-            async (req, _resp) => {
-                if (!(
-                    req.params !== null
-                    && typeof req.params === 'object'
-                    && 'token' in req.params
-                    && typeof req.params.token === 'string'
-                )) {
-                    return httpErrors.badRequest()
-                }
-
-                const token = req.params.token
-
-                const body = acmeTokens[token]
-
-                if (body === undefined) {
-                    return httpErrors.notFound()
-                }
-
-                return body
-            }
-        )
-
-        s.all(
-            '*',
-            {},
-            async (req, resp) => {
-                await resp.redirect(`https://${settings.domain}${req.url}`)
-            }
-        )
+        s.register(fastifyAcmeUnsecurePlugin, { redirectDomain: settings.domain })
     } else {
         await s.register(fastifyCors, {
             methods: ['get', 'post']
@@ -143,10 +90,10 @@ async function initUnsecureServer(production: boolean, acmeTokens: Record<string
 
         const mongo = await MongoStorage.create(s.log.child({ module: 'MONGO' }))
 
-        registerStaticEndpoint(s, 'dist')
         registerAuthEndpoints(s, mongo)
         registerSyncEndpoints(s, mongo)
         registerManagementEndpoints(s, mongo)
+        registerStaticEndpoint(s, 'dist')
     }
 
     await s.listen({
@@ -158,29 +105,19 @@ async function initUnsecureServer(production: boolean, acmeTokens: Record<string
 }
 
 function registerStaticEndpoint<T extends RawServerBase>(server: FastifyInstance<T>, staticFilesPath: string) {
-    server.get('*', async (req, resp) => {
-        const p = req.url
+    server.all('/api/*', async () => {
+        return httpErrors.notFound()
+    })
 
-        if (p.startsWith('/api/')) {
-            throw httpErrors.notFound()
-        }
+    server.register((s) => {
+        const staticFilesPathAbs = path.resolve(staticFilesPath)
+        const indexFilePath = path.join(staticFilesPathAbs, 'index.html')
 
-        const np = path.normalize(path.join(staticFilesPath, p))
-        if (!np.startsWith(`${staticFilesPath}/`)) {
-            throw httpErrors.badRequest('Bad request: wrong path')
-        }
-
-        try {
-            const stat = await lstat(np)
-            if (!stat.isFile()) {
-                throw 'Not found'
-            }
-            await resp.type(mime.lookup(np) || 'application/octet-stream')
-                .send(createReadStream(`./${np}`))
-        } catch {
+        s.register(staticPlugin, { root: staticFilesPathAbs })
+        s.setNotFoundHandler(async (_req, resp) => {
             await resp.type('text/html')
-                .send(createReadStream(`./${staticFilesPath}/index.html`))
-        }
+                .send(createReadStream(indexFilePath))
+        })
     })
 }
 
