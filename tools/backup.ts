676e938114e623032786f9a5bbbd3fd75eecf464
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-01-26 03:47:11 +0300

    Backup/restore tools

---
+++ tools/backup.ts
@@ -0,0 +1,24 @@
+import { mkdir } from 'fs/promises'
+import { DateTime } from 'luxon'
+import { NodeSSH } from 'node-ssh'
+
+const SSH = new NodeSSH()
+
+console.log('Connecting...')
+await SSH.connect({
+    host: '51.250.106.10',
+    username: 'root',
+    privateKeyPath: '/Users/rlz/.ssh/id_rsa'
+})
+
+console.log('Dump data...')
+await SSH.exec('rm', ['-rf', '/cashmony/dump'])
+await SSH.exec('mongodump', ['--gzip', '-d', 'cashmony-app'], { cwd: '/cashmony', stream: 'stderr' })
+
+console.log('Download data...')
+const DIR_NAME = `./backups/${DateTime.utc().toFormat('yyyyMMdd\'T\'HHmmss\'Z\'')}`
+await mkdir(DIR_NAME, { recursive: true })
+await SSH.getDirectory(DIR_NAME, '/cashmony/dump')
+SSH.dispose()
+
+console.log('Done!')
