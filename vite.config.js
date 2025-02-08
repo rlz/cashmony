37b8db8feba8b6ef307f02a69d73b650bd398a79
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-02-08 20:24:12 +0300

    Use VitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,40 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Cashmony - personal finances tracker',
+                short_name: 'Cashmony',
+                description: 'Personal finances tracker',
+                icons: [
+                    {
+                        src: '/favicon.svg',
+                        sizes: 'any'
+                    },
+                    {
+                        src: '/web-app-manifest-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                    {
+                        src: '/web-app-manifest-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ],
+                background_color: '#121212',
+                theme_color: '#121212',
+                scope: '/'
+            }
+        })
+    ],
     root: './front',
     server: {
         port: 3000,
